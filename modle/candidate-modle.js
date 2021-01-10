ec47ff9f6eca6aa0b177654a3953d0756a12097a
ananyae-24 <ananyae123@gmail.com>
2021-01-10 11:49:30 +0530

    ooo

--- modle/candidate-modle.js
+++ modle/candidate-modle.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const schema = new mongoose.Schema(
   {
